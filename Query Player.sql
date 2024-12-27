520110534a20ae79fec79652c259f1ca5fe122a3
Abdur Rahman F R <144410281+abdur7rahman@users.noreply.github.com>
2024-12-27 22:24:46 +0530

    Update Query Player.sql

--- Query Player.sql
+++ Query Player.sql
@@ -2,17 +2,20 @@
 
 SELECT * FROM Player;
 
-+-----+-------------------+-------------+------+--------+------------+-------------------+
-| sno | Player_Name       | Country     | Goal | Assist | Apperences | Goal_Contribution |
-+-----+-------------------+-------------+------+--------+------------+-------------------+
-|   1 | Miroslav Klose    | Germany     |   16 |      4 |         20 |                24 |
-|   2 | Ronaldo Nazário   | Brazil      |   15 |      4 |         19 |                19 |
-|   3 | Lionel Messi      | Argentina   |   13 |      8 |         21 |                26 |
-|   4 | Pelé              | Brazil      |   12 |      5 |         17 |                14 |
-|   5 | Thomas Müller     | Germany     |   12 |      6 |         18 |                16 |
-|   6 | Cristiano Ronaldo | Portugal    |    8 |      2 |         10 |                22 |
-|   7 | Kylian Mbappé     | France      |   12 |      2 |         14 |                14 |
-|   8 | Johan Cruyff      | Netherlands |    3 |      4 |          7 |                 7 |
-|   9 | Zinedine Zidane   | France      |    5 |      2 |          7 |                12 |
-|  10 | Wayne Rooney      | England     |    7 |      3 |         10 |                15 |
-+-----+-------------------+-------------+------+--------+------------+-------------------+
++-----+-------------------+-------------+------+--------+------------+-------------------+--------+
+| sno | Player_Name       | Country     | Goal | Assist | Apperences | Goal_Contribution | Titles |
++-----+-------------------+-------------+------+--------+------------+-------------------+--------+
+|   1 | Miroslav Klose    | Germany     |   16 |      4 |         20 |                24 |      1 |
+|   2 | Ronaldo Nazário   | Brazil      |   15 |      4 |         19 |                19 |      2 |
+|   3 | Lionel Messi      | Argentina   |   13 |      8 |         21 |                26 |      1 |
+|   4 | Pelé              | Brazil      |   12 |      5 |         17 |                14 |      3 |
+|   5 | Thomas Müller     | Germany     |   12 |      6 |         18 |                16 |      1 |
+|   6 | Cristiano Ronaldo | Portugal    |    8 |      2 |         10 |                22 |      0 |
+|   7 | Kylian Mbappé     | France      |   12 |      2 |         14 |                14 |      1 |
+|   8 | Johan Cruyff      | Netherlands |    3 |      4 |          7 |                 7 |      0 |
+|   9 | Zinedine Zidane   | France      |    5 |      2 |          7 |                12 |      1 |
+|  10 | Wayne Rooney      | England     |    7 |      3 |         10 |                15 |      0 |
++-----+-------------------+-------------+------+--------+------------+-------------------+------- +
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
