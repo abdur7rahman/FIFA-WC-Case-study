db612794af1b039df6cc9a50fab0a235844c1a35
Abdur Rahman F R <144410281+abdur7rahman@users.noreply.github.com>
2024-12-27 22:20:03 +0530

    Update Table.sql

--- Table.sql
+++ Table.sql
@@ -27,6 +27,7 @@ CREATE TABLE Player(
    	Goal INT NOT NULL,
    	Assist INT NOT NULL,
    	Apperences INT NOT NULL,
+  	Titles INT NOT NULL,
     UNIQUE(SNo,Player_Name)
 );
 
@@ -46,14 +47,14 @@ INSERT INTO Country VALUES
 
 
 INSERT INTO Player VALUES
-	(1, 'Miroslav Klose', 'Germany', 16, 4, 20, 24),
-	(2, 'Ronaldo Nazário', 'Brazil', 15, 4, 19, 19),
-	(3, 'Lionel Messi', 'Argentina', 13, 8, 21, 26),
-	(4, 'Pelé', 'Brazil', 12, 5, 17, 14),
-	(5, 'Thomas Müller', 'Germany', 12, 6, 18, 16),
-	(6, 'Cristiano Ronaldo', 'Portugal', 8, 2, 10, 22),
-	(7, 'Kylian Mbappé', 'France', 12, 2, 14, 14),
-	(8, 'Johan Cruyff', 'Netherlands', 3, 4, 7, 7),
-	(9,'Zinedine Zidane', 'France', 5, 2, 7, 12),
-	(10, 'Wayne Rooney', 'England', 7, 3, 10, 15);
+	(1, 'Miroslav Klose', 'Germany', 16, 4, 20, 24, 1),
+	(2, 'Ronaldo Nazário', 'Brazil', 15, 4, 19, 19, 2),
+	(3, 'Lionel Messi', 'Argentina', 13, 8, 21, 26, 1),
+	(4, 'Pelé', 'Brazil', 12, 5, 17, 14, 3),
+	(5, 'Thomas Müller', 'Germany', 12, 6, 18, 16, 1),
+	(6, 'Cristiano Ronaldo', 'Portugal', 8, 2, 10, 22, 0),
+	(7, 'Kylian Mbappé', 'France', 12, 2, 14, 14, 1),
+	(8, 'Johan Cruyff', 'Netherlands', 3, 4, 7, 7, 0),
+	(9,'Zinedine Zidane', 'France', 5, 2, 7, 12, 1),
+	(10, 'Wayne Rooney', 'England', 7, 3, 10, 15, 0);
 
